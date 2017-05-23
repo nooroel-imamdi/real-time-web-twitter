b18f20fde249a93836783211e89e34b6206b9439
Nooroel Imamdi <nooroelimamdi@gmail.com>
2017-05-24 00:45:47 +0200

    update counter realtime

--- public/js/script.js
+++ public/js/script.js
@@ -5,49 +5,71 @@
   var tweetCount = document.querySelector('.tweet-count');
   var tweetHashtag = document.querySelector('.tweet-hashtag');
 
-  socket.on('tweet_list_count', function (list) {
-    // console.log(list);
-    // for (var i = 0; i < list.length; i++) {
-    //   tweetCount.innerHTML = list[i].count;
-    //   tweetHashtag.innerHTML = list[i].name;
+  socket.on('tweet_list_count', function (hashtag) {
+    // console.log(hashtag);
+
+    // for (var i = 0; i < hashtag.length; i++) {
+    //   tweetCount.innerHTML = hashtag[i].count;
+    //   tweetHashtag.innerHTML = hashtag[i].name;
     // }
-    // for (var key in list) {
+    // for (var key in hashtag) {
     //   var createList = document.createElement("li");
-    //   var newList = listHashtag.appendChild(createList);
+    //   var newList = hashtagHashtag.appendChild(createList);
     //
-    //   newList = (list[key].count + list[key].name)
+    //   newList = (hashtag[key].count + hashtag[key].name)
 
-      // tweetCount.innerHTML = list[key].count;
+      // tweetCount.innerHTML = hashtag[key].count;
     // }
-    for (var i = 0; i < list.length; i++) {
-      // Update country when their is an update is available.
-      if (list[i].count !== 0) {
+    for (var i = 0; i < hashtag.length; i++) {
+      // Update hashtag when their is an update is available.]
+      if (hashtag[i].count !== 0) {
 
-        console.log(list[i].name , list[i].count, list[i].tweets)
-        // console.log(list)
-        list.forEach(function(e) {
+        // console.log(hashtag[i].name , hashtag[i].count, hashtag[i].tweets)
 
-          var li = document.createElement('li');
-          li.innerHTML = "";
-          li.innerHTML = e.name + " " + e.count;
-          tweetHashtag.appendChild(li);
+        var countUel = document.querySelector('.count-uel');
+        var countUelfinal = document.querySelector('.count-uelfinal');
+        var countAjaman = document.querySelector('.count-ajaman');
+        var countMufc = document.querySelector('.count-mufc');
+        var countAfcajax = document.querySelector('.count-afcajax');
 
-          // li.innerHTML = e.count;
-          // tweetHashtag.appendChild(li)
-          // tweetCount.innerHTML = e.count;
-        })
-        // tweetCount.innerHTML = list[i].count;
-        // tweetHashtag.innerHTML = list[i].name;
+        countUel.innerHTML = hashtag[0].count;
+        countUelfinal.innerHTML = hashtag[1].count;
+        countAjaman.innerHTML = hashtag[2].count;
+        countMufc.innerHTML = hashtag[3].count;
+        countAfcajax.innerHTML = hashtag[4].count;
 
-        // for (var i = 0; i < list.length; i++) {
-        //   tweetCount.innerHTML = list[i].count;
-        //   tweetHashtag.innerHTML = list[i].name;
+        // console.log(hashtag)
+        // hashtag.forEach(function(e) {
+        //   console.log(e);
+        //   var countUel = document.querySelector('.count-uel');
+        //   var countUelfinal = document.querySelector('.count-uelfinal');
+        //   var countAjaman = document.querySelector('.count-ajaman');
+        //   var countMufc = document.querySelector('.count-mufc');
+        //   var countAfcajax = document.querySelector('.count-afcajax');
+        //
+        //
+        //   countMufc.innerHTML = countMufc.innerHTML.replace(e.count);
+        //   // var li = document.createElement('li');
+        //   // li.innerHTML = "";
+        //   // li.innerHTML = e.name + " " + e.count;
+        //   // tweetHashtag.appendChild(li);
+        //
+        //   // li.innerHTML = e.count;
+        //   // tweetHashtag.appendChild(li)
+        //   // tweetCount.innerHTML = e.count;
+        // })
+        // tweetCount.innerHTML = hashtag[i].count;
+        // tweetHashtag.innerHTML = hashtag[i].name;
+
+        // for (var i = 0; i < hashtag.length; i++) {
+        //   tweetCount.innerHTML = hashtag[i].count;
+        //   tweetHashtag.innerHTML = hashtag[i].name;
         // }
       }
     }
 
-    // tweetCount.innerHTML = list;
-    // tweetHashtag.innerHTML = list.name;
+    // tweetCount.innerHTML = hashtag;
+    // tweetHashtag.innerHTML = hashtag.name;
   });
 
   // Error handler
